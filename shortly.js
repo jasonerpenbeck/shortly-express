2e689c924f192e1b126dd537144a5be535a78e6c
jasonerpenbeck <jason.erpenbeck@gmail.com>
2014-10-07 16:52:03 -0700

    Working on knex queries for authentication

--- shortly.js
+++ shortly.js
@@ -3,10 +3,9 @@ var util = require('./lib/utility');
 var partials = require('express-partials');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+var cookieparser = require('cookie-parser');
 var querystring = require('querystring');
 var bcrypt = require('bcrypt-nodejs');
-
-
 
 var db = require('./app/config');
 var Users = require('./app/collections/users');
@@ -14,9 +13,35 @@ var User = require('./app/models/user');
 var Links = require('./app/collections/links');
 var Link = require('./app/models/link');
 var Click = require('./app/models/click');
-var store = {};
 
 var app = express();
+var validSessions = {};
+var SESSIONEXP = 3000000;
+
+var checkUser = function(req, res, next){
+  console.log(req.url);
+  console.log('user has requested a protected resource, "' + req.url + '"');
+  console.log('Authenticating');
+  console.log('ValidSessions Object: ',validSessions);
+  console.log('Session ID from request: ',req.sessionID);
+
+  if (validSessions.hasOwnProperty(req.sessionID)){
+    console.log('Valid Session Exists');
+    if (Date.now() - validSessions[req.sessionID] < SESSIONEXP){
+      //USER HAS A VALID SESSION
+      console.log('User is within the window, advancing');
+      res.redirect('links');
+    } else {
+      //USER SESSION HAS EXPIRED
+      console.log('User session has expired');
+    }
+  }
+  console.log('User is not authenticated and is being redirected to /login');
+  //res.end('not authenticated');
+  res.redirect('login');
+  //redirect to login if not authenticated, otherwise next();
+};
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -26,56 +51,44 @@ app.use(bodyParser.json());
 // Parse forms (signup/login)
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-app.use(session({secret: 'nyan cat'}));
+app.use(cookieparser());
+app.use(session({secret: 'This should be working.'}));
 
-app.use('/create', function(req, res, next){
-  var sessionID = querystring.parse(req.headers.cookie)['connect.sid'] || req.sessionID;
+// app.use('/', checkUser);
+// app.use('/create', checkUser);
+// app.use('/links', checkUser);
 
-  console.log(req.sessionID);
-  console.log(store);
-  if (store.hasOwnProperty(sessionID)){
-    console.log("Already authenticated")
-    next();
-  } else {
-    console.log('Hi. Not authenticated');
-    res.render('login');
-  }
-});
-
-app.get('/',
-function(req, res) {
+app.get('/create', checkUser, function(req, res) {
+  console.log('user has requested to get "/create"');
   res.render('index');
 });
 
-app.get('/create',
-function(req, res) {
-  //if authenticated
+app.get('/', checkUser, function(req, res) {
+  console.log('user has requested to get "/"');
   res.render('index');
 });
 
-app.get('/login',
-function(req, res) {
-  //if authenticated
+app.get('/login', function(req, res) {
+  console.log('user has requested to get "/login"');
   res.render('login');
 });
 
 app.get('/signup',
 function(req, res) {
-  //if authenticated
+  console.log('user has requested to get "/signup"');
   res.render('signup');
 });
 
-app.get('/links',
-function(req, res) {
+app.get('/links', checkUser, function(req, res) {
+  console.log('user has requested to get "/links"');
+  console.log('Session ID from request within app.get links: ',req.sessionID);
   Links.reset().fetch().then(function(links) {
     res.send(200, links.models);
   });
 });
 
-app.post('/links',
-function(req, res) {
+app.post('/links', checkUser, function(req, res) {
   var uri = req.body.url;
-
   if (!util.isValidUrl(uri)) {
     console.log('Not a valid url: ', uri);
     return res.send(404);
@@ -106,9 +119,18 @@ function(req, res) {
   });
 });
 
+/************************************************************/
+// Write your authentication routes here
+/************************************************************/
+app.get('/logout', function(req, res) {
+  console.log('user has requested to get "/logout"');
+  req.session.destroy(); // check syntax
+    res.redirect('/login');
+});
+
 app.post('/signup', function(req) {
-  var formData = "";
-  console.log(req.body);
+  console.log('user has requested to post to "/signup"');
+  // console.log(req.body);
   var credentials = {};
   credentials.username = req.body.username;
   credentials.password = req.body.password;
@@ -116,81 +138,53 @@ app.post('/signup', function(req) {
   console.log('Credentials: ',credentials);
   db.knex('users').insert({
     username: credentials.username,
-    password: bcrypt.hashSync(credentials.password),
+    password: credentials.password,
     created_at: Date.now(),
-    updated_at: Date.now()}).then(function(stuff) {
-      console.log(stuff);
+    updated_at: Date.now()}).then(function(rows) {
+      console.log(rows);
       console.log('Hi.  You now exist in our database.');
       db.knex.raw('select * from users where id = ?', [16]).then(function(resp) {
         console.log('select response for this user',resp);
       });
-
     });
-
-
-
 });
 
-app.post('/login', function(req) {
-  var formData = "";
-  console.log(req.body);
+app.post('/login', function(req, res) {
   var credentials = {};
   credentials.username = req.body.username;
   credentials.password = req.body.password;
 
+  // Run this to delete all rows from users
+  // db.knex('users').del().then(function() {
+  //   console.log("Rows have been deleted");
+  // });
+
+  // Run this to select all rows from users
+  // db.knex('users').select().then(function(allRows) {
+  //   console.log(allRows);
+  // });
+
   console.log('Credentials: ',credentials);
+
   db.knex('users').where({
     username: credentials.username,
-    password: bcrypt.hashSync(credentials.password)}).then(function() {
-      console.log('Hi.  You are authentic.');
+    password: credentials.password})
+  .then(function(rows) {
+    console.log('Rows: ',rows);
+    // console.log('Hi. You are authentic.');
+    validSessions[req.sessionID] = {
+      authDate: Date.now(),
+      userID: 1, // get userID from database query
+      lastPage: []
+    };
+    console.log('ValidSessions: ',validSessions, ' | ', req.sessionID);
+    res.redirect('/links');
+  })
+  .catch(function(error) {
+    console.log('Error: ',error);
+  });
 
-    });
-
-
-  // req.on('data', function(chunk){
-  //   console.log('getting data');
-  //   formData += chunk;
-  //   console.log(formData);
-  // });
-
-  // req.on('end', function(){
-  //   // var credentials = querystring.parse(formData);
-  //   console.log('Credentials: ',credentials);
-  //   db.knex(users).where({
-  //     username: credentials.username,
-  //     password: magicBCrypypHash(credentials.password)
-  //   });
-  // });
 });
-/************************************************************/
-// Write your authentication routes here
-/************************************************************/
-
-
-
-
-// /login
-//  // on success, go to links page
-//  // on error, issue app.get('/') with an error message (username/password combo is not found)
-//
-// /signup
-//  // provide username & password
-//  // 1. check if username already exists
-//      // if yes, check if password matches
-//        // if passsword matches, log them in, go to links page
-//      // if no, issue app.get('/') with an error message (username exists)
-//     2. if username does not exist
-//      // create an entry in database
-//      // encrypt password, etc.
-//     3. on success, go to links page (which will be blank)
-//
-//
-// /logout
-//
-
-
-
-
 
 /************************************************************/
 // Handle the wildcard route last - if all other routes fail
